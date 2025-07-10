eec3bfedaac55b50cdc9567c026c8e1e4ff4b040
Jeremy <jeremy0603@gmail.com>
2025-07-10 20:07:50 +0800

    chore: 新增 GitHub Pages 部署工作流程

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue3-order-book/',
   plugins: [
     vue(),
     vueDevTools(),
